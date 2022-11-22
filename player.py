94f1171ce54c4ca40f759898ac3a45b2561052c4
huwng22 <105183757+huwng22@users.noreply.github.com>
2022-11-22 07:03:07 +0700

    Update player.py

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 from tkinter import *
+from data import *
 
 #Board Size
-Size_window_width = 1200
-Size_window_height = 1000
+Size_window_width = 800
+Size_window_height = 800
 Board_Size = 15
 Frame_Gap = 35
-width = 1000
-height = 1000
+width = 800
+height = 800
 
 #Board
 Board_Size = Board_Size - 1
@@ -18,23 +19,28 @@ Board_GapY = (height - Board_Y1 * 2) / Board_Size
 #Chess Piece
 Chess_Radius = (Board_GapX * (9 / 10)) / 4
 
-#Turn
-
+#2D Board List
+board_list = []
+#2D list for gameboard
+for i in range(Board_Size + 1):
+    board_list.append([0] * (Board_Size + 1))
 
 class Player(Entry,Button):
 
     def __init__(self):
         super().__init__()
         self.turn = 1
-        self.turn_fill = ""
+        self.turn_fill = "white"
         self.name_person = ""
         #self.name_person2 = ""
         self.type_person = ""
         self.entry_name = Entry(width = 15)
         #self.entry_name2 = Entry(width = 15)
+        
+        self.X = None
+        self.Y = None
 
-
-    def Player_turn(self,canvas,Create_piece):
+    def Player_turn(self,canvas):
 
         Click_Cord = [None, None]
 
@@ -45,8 +51,7 @@ class Player(Entry,Button):
         Actual_CordY1 = []
         Actual_CordX2 = []
         Actual_CordY2 = []
-        #2D Board List
-        board = []
+        
 
         def Value_Check_int(Value):
             try:
@@ -57,54 +62,38 @@ class Player(Entry,Button):
                 return "int"
 
         def MouseClick(event):
-            global Click_Cord
-            def Value_Check_int(Value):
-                try:
-                    Value = int(Value)
-                except ValueError:
-                    return "string"
-                else:
-                    return "int"
-
+            #global Click_Cord
             X_click = event.x
             Y_click = event.y
             Click_Cord = Piece_Location(X_click, Y_click)
             print(Click_Cord)
-            X = Click_Cord[0]
-            Y = Click_Cord[1]
+            self.X = Click_Cord[0]
+            self.Y = Click_Cord[1]
 
-            if self.turn % 2 == 1:
-                self.turn_fill = "red"
-            else:
-                self.turn_fill = "green"
+            # if self.turn % 2 == 1:
+            #     self.turn_fill = "red"
+            # else:
+            #     self.turn_fill = "green"
             
-            Create_piece(Board_X1 + Board_GapX * (X - 1), Board_Y1 + Board_GapY * (Y - 1),fill = self.turn_fill)
-            print(self.turn_fill)
-            self.turn += 1
+            #Create_piece(Board_X1 + Board_GapX * (self.X - 1), Board_Y1 + Board_GapY * (self.Y - 1),fill = self.turn_fill)
+            #print(self.turn_fill)
+            #self.turn += 1
 
 
         canvas.bind("<Button-1>", MouseClick)
         
         def Piece_Location(X_click, Y_click):    
-            X = None
-            Y = None
+            self.X = None
+            self.Y = None
             for i in range(len(Actual_CordX1)):
         
                 if X_click > Actual_CordX1[i] and X_click < Actual_CordX2[i]:
-                    X = Game_CordX[i]
+                    self.X = Game_CordX[i]
 
                 if Y_click > Actual_CordY1[i] and Y_click < Actual_CordY2[i]:
-                    Y = Game_CordY[i]
+                    self.Y = Game_CordY[i]
 
-            return X, Y
-
-        def Location_Validation():
-
-            if X == None or Y == None:
-                return False
-        
-            elif board[Y - 1][X - 1] == 0:
-                return True
+            return self.X, self.Y
 
         for z in range(1, Board_Size + 2):
             for i in range(1, Board_Size + 2):
@@ -116,6 +105,27 @@ class Player(Entry,Button):
                 Actual_CordY2.append((i - 1) * Board_GapY + Board_Y1 + Chess_Radius)
 
         canvas.update()
+
+        if self.X == None or self.Y == None:
+                return False
+        
+        elif board_list[self.Y - 1][self.X - 1] == 0:
+            return True
+    
+    def Location_Validation(self):
+        pass
+            # X_click = event.x
+            # Y_click = event.y
+            # Click_Cord = Piece_Location(X_click, Y_click)
+            # print(Click_Cord)
+            # self.X = Click_Cord[0]
+            # self.Y = Click_Cord[1]
+
+            # if self.X == None or self.Y == None:
+            #     return False
+        
+            # elif board_list[self.Y - 1][self.X - 1] == 0:
+            #     return True
 
                 
         
